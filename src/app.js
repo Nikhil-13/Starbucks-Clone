a829ed3d657471619560328569c230721352c263
Nikhil-13 <nnik270@gmail.com>
2023-02-28 13:22:22 +0530

    added ham menu

--- src/app.js
+++ src/app.js
@@ -4,3 +4,9 @@ list.forEach((ul) => {
     e.target.parentElement.parentElement.parentElement.classList.toggle("active");
   });
 });
+
+const ham = document.querySelector(".hamburger");
+ham.addEventListener("click", (e) => {
+  e.target.parentElement.classList.toggle("active");
+  document.querySelector(".navbar").classList.toggle("active");
+});
